3eb5aed6e8f3d1e7034648e5ab7dbfc8f1c448d9
Ray Krueger <raykrueger@gmail.com>
2025-02-10 23:02:59 +0000

    Example deployment and cleanup.sh

--- manifests/modules/networking/eks-hybrid-nodes/.workshop/cleanup.sh
+++ manifests/modules/networking/eks-hybrid-nodes/.workshop/cleanup.sh
@@ -3,3 +3,11 @@
 set -e
 
 logmessage "Cleaning up EKS Hybrid Nodes Module"
+
+kubectl delete deployment nginx-deployment || true
+
+helm uninstall cilium --ignore-not-found || true
+
+helm repo remove cilium || true
+
+kubectl delete nodes -l eks.amazonaws.com/compute-type=hybrid || true
